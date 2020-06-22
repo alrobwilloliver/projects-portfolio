f61c57ffec70d8047a60cd368882ff9bc76d7b83
Alan Oliver <alrobwilloliver@gmail.com>
2020-06-22 03:34:02 +0100

    added join static files middeware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
     res.render("index.html")
 })
