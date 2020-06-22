db1a80702403214f98fc057e09c0f6b4627d0fd7
Alan Oliver <alrobwilloliver@gmail.com>
2020-06-22 03:26:23 +0100

    added get req to index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.render("index.html")
+})
+
 app.listen(8000, () => {
     console.log("Connected");
 })
